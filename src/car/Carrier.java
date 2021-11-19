acac64acd3fbda2901d8567b909d7cd112dbc637
SnapzTry <max.e.levin@gmail.com>
2021-11-19 11:44:30 +0100

    Changes by sarahedb

--- src/car/Carrier.java
+++ src/car/Carrier.java
@@ -1,6 +1,7 @@
 package car;
 
 import java.awt.*;
+import java.util.Stack;
 
 /**
  * Class Carrier is a
@@ -18,17 +19,106 @@ public class Carrier extends Car {
         modelName = "truck.Carrier";
         rampUp = true;
         stopEngine();
-    }
-    @Override
-    double speedFactor() {
-        return 0;
+
     }
 
+    /**
+     * Method calls the helper class Truck's implementation and sends necessary arguments.
+     * @return Double with the value enginePower times the constant 0.004.
+     */
     @Override
-    void incrementSpeed(double amount) {}
+    double speedFactor() { return helper.speedFactor(enginePower); }
 
+    /**
+     * Method calls the helper class Truck's implementation and sends necessary arguments.
+     * @param amount Double in the interval [0, 1]
+     */
     @Override
-    void decrementSpeed(double amount) {}
+    void incrementSpeed(double amount) { currentSpeed = helper.incrementSpeed(amount, currentSpeed, enginePower); }
+
+    /**
+     * Method calls the helper class Truck's implementation and sends necessary arguments.
+     * @param amount Double in the interval [0, 1]
+     */
+    @Override
+    void decrementSpeed(double amount) { currentSpeed = helper.decrementSpeed(amount, currentSpeed, enginePower); }
+
+    /**
+     * Method lowers the ramp by making the rampUp boolean False.
+     * Method prints an error message if the carrier isn't stationary when calling.
+     */
+    public void raiseRamp (){
+        if (currentSpeed == 0) {
+            rampUp = true;
+        }else {
+            System.out.println("*pip sound* Please ensure carrier is stationary before raising the ramp");
+        }
+    }
+
+    /**
+     * Method lowers the ramp by making the rampUp boolean False.
+     * Method prints an error message if the carrier isn't stationary when calling.
+     */
+    public void lowerRamp (){
+        if (currentSpeed == 0) {
+            rampUp = false;
+        }else {
+            System.out.println("*pip sound* Please ensure carrier is stationary before lowering the ramp");
+        }
+    }
+
+    private boolean carProximity (Car carToCheck) {
+        return (carToCheck.getX() - getX() <= 3) && (carToCheck.getY() - getY() <= 3);
+    }
+
+    /**
+     * Method loads a car to the carrier if carProximity is true and the loaded vehicle has the type Car.
+     * @param carToLoad The parameter has the type Car.
+     */
+    public void loadCar (Car carToLoad) {
+        if (carProximity(carToLoad) && carToLoad.getClass() != Carrier.class) {
+            loadedCars.push(carToLoad);
+        }else {
+            System.out.println("Please drive closer before loading a car onto the carrier");
+        }
+
+    }
+    @Override
+    public void move(){
+        super.move();
+        if (loadedCars.size() > 0)
+            for (Car car: loadedCars){
+                car.setX(getX());
+                car.setY(getY());
+            }
+    }
+
+    public void unloadCar () {
+        if (!rampUp){
+            Car unloadedCar = loadedCars.pop();
+            switch(getDirection()) {
+                case UP -> {
+                    unloadedCar.setX(getX());
+                    unloadedCar.setY(getY() - 1);
+                }
+                case LEFT -> {
+                    unloadedCar.setX(getX() + 1);
+                    unloadedCar.setY(getY());
+                }
+                case RIGHT -> {
+                    unloadedCar.setX(getX() - 1);
+                    unloadedCar.setY(getY());
+                }
+                case DOWN -> {
+                    unloadedCar.setX(getX());
+                    unloadedCar.setY(getY() + 1);
+                }
+            }
+        } else {
+            System.out.println("Please lower the ramp before attempting to unload a car");
+        }
+
+    }
 }
 
 
