d3b2e9994dcf14be6455d1e45147b040cb5978a6
SnapzTry <max.e.levin@gmail.com>
2021-12-06 16:35:13 +0100

    Changes by oyoun

--- src/paket/car/Truck.java
+++ src/paket/car/Truck.java
@@ -12,25 +12,5 @@ public abstract class Truck extends Car{
     @Override
     double speedFactor() { return 0.004 * enginePower; }
 
-    /**
-     * Method updates the instance variable current speed with a calculation that increases it.
-     * Can't set current speed to a value higher than enginePower variable's value.
-     * @param amount Double between interval [0,1].
-     */
-    @Override
-    void incrementSpeed(double amount) {
-        currentSpeed = Math.min(currentSpeed + speedFactor() * amount, enginePower);
-    }
-
-    /**
-     * Method updates the instance variable current speed with a calculation that decreases it.
-     * Can't set current speed to a value lower than Zero.
-     * @param amount Double between interval [0,1].
-     * @return currentSpeed Double representing the cars speed
-     */
-    @Override
-    void decrementSpeed(double amount){
-        currentSpeed = Math.max(currentSpeed - speedFactor() * amount, 0);
-    }
 }
 
--- src/paket/car/Volvo240.java
+++ src/paket/car/Volvo240.java
@@ -37,24 +37,4 @@ public class Volvo240 extends Car{
         return enginePower * 0.01 * trimFactor;
     }
 
-    /**
-     * Used by the method gas to calculate the new currentSpeed of a Volvo240.
-     * Cannot return a value that is larger than enginePowers value.
-     * @param amount a double representing the gas amount, between 0 and 1.
-     */
-    @Override
-    void incrementSpeed(double amount) {
-        currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount, enginePower);
-    }
-
-    /**
-     * Used by the method brake to calculate the new currentSpeed of a saab95.
-     * Cannot return a value that is lower than 0.
-     * @param amount a double representing the brake amount, between 0 and 1.
-     */
-    @Override
-    void decrementSpeed(double amount) {
-        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount, 0);
-    }
-
 }
